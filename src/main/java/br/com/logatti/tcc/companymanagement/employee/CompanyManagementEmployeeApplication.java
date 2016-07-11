a4352855b257018dcb1a85bd6cffd09c1caaeb13
renan machado <renan machado>
2016-07-11 17:44:02 -0300

    Push initial project

---
+++ src/main/java/br/com/logatti/tcc/companymanagement/employee/CompanyManagementEmployeeApplication.java
@@ -0,0 +1,12 @@
+package br.com.logatti.tcc.companymanagement.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyManagementEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyManagementEmployeeApplication.class, args);
+	}
+}
